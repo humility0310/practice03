4dde6222707969368a61bce5aac924e9cefa9d6f
yj <bit-user@bit>
2016-09-19 15:32:05 +0900

    3번완

--- src/prob3/CurrencyConverter.java
+++ src/prob3/CurrencyConverter.java
@@ -1,18 +1,20 @@
 package prob3;
 
 public class CurrencyConverter {
-	private static int rate;
-
+	public static double rate;
+	
 	public static double toDollar(double won) {
 		// 한국 원화를 달러로 변환
+		return won*rate/1000;
 	}
 
 	public static double toKRW(double dollar) {
-		// 달러를 한국 원화로 변환
+		return dollar/rate*1000000;
 	}
 
 	public static void setRate(double r) {
 		// 환율 설정(KRW/$1)
+		rate = r*0.89;
 
 	}
 }
--- src/prob3/Prob3.java
+++ src/prob3/Prob3.java
@@ -5,8 +5,8 @@ public class Prob3 {
 	public static void main(String[] args) {
 		CurrencyConverter.setRate( 1000 );
 		
-		double dollar = CurrencyConverter.toDollar( 1000000 );
-		System.out.println( "백만원은 " + dollor + "달러입니다." );
+		double dallar = CurrencyConverter.toDollar( 1000000 );
+		System.out.println( "백만원은 " + dallar + "달러입니다." );
 		
 		double krw = CurrencyConverter.toKRW( 100 );
 		System.out.println( "백달러는 " + krw + "원 입니다." );		
